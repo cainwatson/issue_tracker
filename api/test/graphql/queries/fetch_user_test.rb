00b647f07d2b8d511b05195338e510a080395fb6
Cain Watson <cainwatson00@gmail.com>
2020-08-08 18:27:22 -0500

    test: check fetch_user with invalid id

--- api/test/graphql/queries/fetch_user_test.rb
+++ api/test/graphql/queries/fetch_user_test.rb
@@ -20,4 +20,22 @@ class FetchUserQueryTest < ActiveSupport::TestCase
     assert_equal user_result["createdAt"], user.created_at.iso8601
     assert_equal user_result["updatedAt"], user.updated_at.iso8601
   end
+
+  test "returns null with invalid id" do
+    query_string = <<-GRAPHQL
+      query fetch_user($id: ID!) {
+        user(id: $id) {
+          id
+          createdAt
+          updatedAt
+        }
+      }
+    GRAPHQL
+
+    user = users[0]
+    result = IssueTrackerSchema.execute(query_string, variables: {"id" => 0})
+    user_result = result["data"]["user"]
+
+    assert_nil user_result
+  end
 end
