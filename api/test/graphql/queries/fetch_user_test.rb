3d0c885250dd4fab85a476662f4005c541084e41
Cain Watson <cainwatson00@gmail.com>
2020-08-08 18:20:52 -0500

    test: fetch user query

---
+++ api/test/graphql/queries/fetch_user_test.rb
@@ -0,0 +1,23 @@
+require "test_helper"
+
+class FetchUserQueryTest < ActiveSupport::TestCase
+  test "returns user with id" do
+    query_string = <<-GRAPHQL
+      query fetch_user($id: ID!) {
+        user(id: $id) {
+          id
+          createdAt
+          updatedAt
+        }
+      }
+    GRAPHQL
+
+    user = users[0]
+    result = IssueTrackerSchema.execute(query_string, variables: {"id" => user.id})
+    user_result = result["data"]["user"]
+
+    assert_equal user_result["id"], user.id.to_s
+    assert_equal user_result["createdAt"], user.created_at.iso8601
+    assert_equal user_result["updatedAt"], user.updated_at.iso8601
+  end
+end
