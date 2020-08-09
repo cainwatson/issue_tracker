e63062490b1c40beb02db6bc424e7bb67a356b75
Cain Watson <cainwatson00@gmail.com>
2020-08-09 16:48:51 -0500

    chore: add more seeds

--- api/db/seeds.rb
+++ api/db/seeds.rb
@@ -9,3 +9,37 @@
 5.times do |num|
   Accounts::User.create()
 end
+
+users = Accounts::User.all().to_a
+
+organizations = users.map.with_index do |user, index|
+  Organizations::Organization.create(
+    name: "Org #{index + 1}",
+    user_creator: user
+  )
+end
+
+projects = organizations.flat_map.with_index do |organization, index|
+  [
+    Projects::Project.create(
+      name: "Project #{index + 1}",
+      owner: organization,
+      user_creator: organization.user_creator
+    ),
+    Projects::Project.create(
+      name: "User Project #{index + 1}",
+      owner: organization.user_creator,
+      user_creator: organization.user_creator
+    )
+  ]
+end
+
+projects.each do |project|
+  4.times do |num|
+    Projects::Issue.create(
+      summary: "Issue #{num}",
+      project: project,
+      user_creator: project.user_creator
+    )
+  end
+end
