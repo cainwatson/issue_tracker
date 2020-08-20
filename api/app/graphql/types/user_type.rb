54838f752ac03cba21a17536752e0c4bc9dccd1a
Cain Watson <cainwatson00@gmail.com>
2020-08-20 18:48:35 -0500

    feat: add projects field to user type

--- api/app/graphql/types/user_type.rb
+++ api/app/graphql/types/user_type.rb
@@ -2,6 +2,7 @@ module Types
   class UserType < Types::BaseObject
     field :id, ID, null: false
     field :profile, Types::ProfileType, null: false
+    field :projects, [Types::ProjectType], null: false
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
   end
