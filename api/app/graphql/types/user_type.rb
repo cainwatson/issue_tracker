cc4996fd53cbde04e2dc05d5845098c9f5822393
Cain Watson <cainwatson00@gmail.com>
2020-08-20 17:20:52 -0500

    feat: add profile to user graphql type

--- api/app/graphql/types/user_type.rb
+++ api/app/graphql/types/user_type.rb
@@ -1,6 +1,7 @@
 module Types
   class UserType < Types::BaseObject
     field :id, ID, null: false
+    field :profile, Types::ProfileType, null: false
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
   end
