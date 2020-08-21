8f3926b04c84d6c6c2deba25be8e5497aadab410
Cain Watson <cainwatson00@gmail.com>
2020-08-20 19:21:10 -0500

    refactor: adjust user projects query test

--- api/test/graphql/queries/fetch_user_projects_test.rb
+++ api/test/graphql/queries/fetch_user_projects_test.rb
@@ -3,7 +3,7 @@ require "test_helper"
 class FetchUserProjectsQueryTest < ActiveSupport::TestCase
   test "returns the user's projects" do
     query_string = <<-GRAPHQL
-      query fetchUserprojects($id: ID!) {
+      query fetchUserProjects($id: ID!) {
         user(id: $id) {
           id
           projects {
@@ -11,9 +11,7 @@ class FetchUserProjectsQueryTest < ActiveSupport::TestCase
             name
             photoUrl
             isPrivate
-            userCreatorId
             ownerType
-            ownerId
             createdAt
             updatedAt
           }
@@ -22,23 +20,20 @@ class FetchUserProjectsQueryTest < ActiveSupport::TestCase
     GRAPHQL
 
     user = accounts_users(:megan)
-    user_projects = [projects_projects(:megans)]
 
     result = IssueTrackerSchema.execute(query_string, variables: {"id" => user.id})
     user_result = result["data"]["user"]
 
-    assert_equal user_result["projects"], [
-      {
-        "id" => user_projects[0].id,
-        "name" => user_projects[0].name,
-        "photoUrl" => user_projects[0].photoUrl,
-        "isPrivate" => user_projects[0].isPrivate,
-        "userCreatorId" => user_projects[0].userCreatorId,
-        "ownerType" => user_projects[0].ownerType,
-        "ownerId" => user_projects[0].ownerId,
-        "createdAt" => user_projects[0].createdAt,
-        "updatedAt" => user_projects[0].updatedAt,
-      },
-    ]
+    user.projects.each_with_index do |project, index|
+      project_result = user_result["projects"][index]
+
+      assert project_result["id"]
+      assert project_result["createdAt"]
+      assert project_result["updatedAt"]
+      assert_equal project_result["ownerType"], "ACCOUNT"
+      assert_equal project_result["name"], project.name
+      assert_equal project_result["photoUrl"], project.photo_url
+      assert_equal project_result["isPrivate"], project.is_private
+    end
   end
 end
