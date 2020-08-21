2622b0cad4fcab29f41cbf1f5a64c4df9139ee72
Cain Watson <cainwatson00@gmail.com>
2020-08-20 21:36:24 -0500

    feat: change organization type to implement node

--- api/app/graphql/types/organization_type.rb
+++ api/app/graphql/types/organization_type.rb
@@ -1,11 +1,16 @@
 module Types
   class OrganizationType < Types::BaseObject
-    field :id, ID, null: false
+    implements GraphQL::Types::Relay::Node
+
+    global_id_field :id
+
+    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+
     field :name, String, null: false
     field :photo_url, String, null: true
     field :user_creator_id, Integer, null: false
+
     field :projects, [Types::ProjectType], null: false
-    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
-    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
   end
 end
