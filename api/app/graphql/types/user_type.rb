b74782066ad1179a9de79956ab48a8660ce25e13
Cain Watson <cainwatson00@gmail.com>
2020-08-20 21:36:17 -0500

    feat: change user type to implement node

--- api/app/graphql/types/user_type.rb
+++ api/app/graphql/types/user_type.rb
@@ -1,10 +1,14 @@
 module Types
   class UserType < Types::BaseObject
-    field :id, ID, null: false
+    implements GraphQL::Types::Relay::Node
+
+    global_id_field :id
+
+    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+
     field :profile, Types::ProfileType, null: false
     field :projects, [Types::ProjectType], null: false
     field :memberships, [Types::MembershipType], null: false
-    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
-    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
   end
 end
