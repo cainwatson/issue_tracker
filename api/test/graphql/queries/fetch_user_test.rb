a6e15cea9afd12235e72e1dfa3ce571a29cdb888
Cain Watson <cainwatson00@gmail.com>
2020-08-21 00:29:13 -0500

    refactor: simplify fetch user tests

--- api/test/graphql/queries/fetch_user_test.rb
+++ api/test/graphql/queries/fetch_user_test.rb
@@ -51,10 +51,6 @@ class FetchUserQueryTest < ActiveSupport::TestCase
             profile {
               id
               firstName
-              lastName
-              photoUrl
-              createdAt
-              updatedAt
             }
           }
         }
@@ -67,12 +63,9 @@ class FetchUserQueryTest < ActiveSupport::TestCase
     result = IssueTrackerSchema.execute(query_string, variables: { 'id' => user.node_id })
     user_result = result['data']['node']
 
+    assert user_result['profile']
     assert user_result['profile']['id']
-    assert user_result['profile']['createdAt']
-    assert user_result['profile']['updatedAt']
     assert_equal user_result['profile']['firstName'], user_profile.first_name
-    assert_equal user_result['profile']['lastName'], user_profile.last_name
-    assert_equal user_result['profile']['photoUrl'], user_profile.photo_url
   end
 
   test 'returns user with projects' do
@@ -84,11 +77,6 @@ class FetchUserQueryTest < ActiveSupport::TestCase
             projects {
               id
               name
-              photoUrl
-              isPrivate
-              ownerType
-              createdAt
-              updatedAt
             }
           }
         }
@@ -100,16 +88,14 @@ class FetchUserQueryTest < ActiveSupport::TestCase
     result = IssueTrackerSchema.execute(query_string, variables: { 'id' => user.node_id })
     user_result = result['data']['node']
 
+    assert_equal user_result['projects'].length, user.projects.length
+
     user.projects.each_with_index do |project, index|
       project_result = user_result['projects'][index]
 
+      assert project_result
       assert project_result['id']
-      assert project_result['createdAt']
-      assert project_result['updatedAt']
-      assert_equal project_result['ownerType'], 'ACCOUNT'
       assert_equal project_result['name'], project.name
-      assert_equal project_result['photoUrl'], project.photo_url
-      assert_equal project_result['isPrivate'], project.is_private
     end
   end
 
@@ -121,21 +107,6 @@ class FetchUserQueryTest < ActiveSupport::TestCase
             id
             memberships {
               id
-              createdAt
-              updatedAt
-              userFrom {
-                id
-              }
-              userTo {
-                id
-              }
-              organization {
-                id
-                name
-                photoUrl
-                createdAt
-                updatedAt
-              }
             }
           }
         }
@@ -147,21 +118,13 @@ class FetchUserQueryTest < ActiveSupport::TestCase
     result = IssueTrackerSchema.execute(query_string, variables: { 'id' => user.node_id })
     user_result = result['data']['node']
 
-    user.memberships.each_with_index do |membership, index|
+    assert_equal user_result['memberships'].length, user.memberships.length
+
+    user.memberships.each_with_index do |_membership, index|
       membership_result = user_result['memberships'][index]
 
+      assert membership_result
       assert membership_result['id']
-      assert membership_result['userFrom']
-      assert membership_result['userTo']
-      assert membership_result['organization']
-      assert membership_result['createdAt']
-      assert membership_result['updatedAt']
-
-      assert membership_result['organization']['id']
-      assert membership_result['organization']['createdAt']
-      assert membership_result['organization']['updatedAt']
-      assert_equal membership_result['organization']['name'], membership.organization.name
-      assert_equal membership_result['organization']['photoUrl'], membership.organization.photo_url
     end
   end
 end
