0dddf534aa07c327bfff4eff655abefa92c4493f
Cain Watson <cainwatson00@gmail.com>
2020-08-23 19:44:02 -0500

    feat: install graphql playground plugin

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  post '/graphql', to: 'graphql#execute'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  post '/graphql', to: 'graphql#execute'
+  mount GraphQL::Playground::Engine, at: '/graphql-playground', graphql_path: '/graphql' if Rails.env.development?
 end
