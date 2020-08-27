f3fc43251ce1708a321d4808d3a0eb844e1b4caa
Cain Watson <cainwatson00@gmail.com>
2020-08-27 18:41:47 -0500

    test: fetch board query

---
+++ api/test/graphql/queries/fetch_board_test.rb
@@ -0,0 +1,93 @@
+require 'test_helper'
+
+class FetchBoardQueryTest < ActiveSupport::TestCase
+  test 'returns null with invalid id' do
+    query_string = <<-GRAPHQL
+      query fetchBoard($id: ID!) {
+        node(id: $id) {
+          ...on Board {
+            id
+            createdAt
+            updatedAt
+            name
+          }
+        }
+      }
+    GRAPHQL
+
+    result = graphql_query(query_string, variables: { 'id' => 'invalid' })
+    board_result = result['data']['node']
+
+    assert_nil board_result
+  end
+
+  test 'returns board with id' do
+    query_string = <<-GRAPHQL
+      query fetchBoard($id: ID!) {
+        node(id: $id) {
+          ...on Board {
+            id
+            createdAt
+            updatedAt
+            name
+          }
+        }
+      }
+    GRAPHQL
+
+    board = projects_boards(:tylers_one)
+    result = graphql_query(query_string, variables: { 'id' => board.node_id })
+    board_result = result['data']['node']
+
+    assert board_result['id']
+    assert board_result['createdAt']
+    assert board_result['updatedAt']
+    assert_equal board_result['name'], board.name
+  end
+
+  test 'returns board with user creator' do
+    query_string = <<-GRAPHQL
+      query fetchBoard($id: ID!) {
+        node(id: $id) {
+          ...on Board {
+            id
+            userCreator {
+              id
+            }
+          }
+        }
+      }
+    GRAPHQL
+
+    board = projects_boards(:tylers_one)
+    result = graphql_query(query_string, variables: { 'id' => board.node_id })
+    board_result = result['data']['node']
+
+    assert board_result['userCreator']
+    assert board_result['userCreator']['id']
+  end
+
+  test 'returns board with project' do
+    query_string = <<-GRAPHQL
+      query fetchBoard($id: ID!) {
+        node(id: $id) {
+          ...on Board {
+            id
+            project {
+              id
+              name
+            }
+          }
+        }
+      }
+    GRAPHQL
+
+    board = projects_boards(:tylers_one)
+    result = graphql_query(query_string, variables: { 'id' => board.node_id })
+    board_result = result['data']['node']
+
+    assert board_result['project']
+    assert board_result['project']['id']
+    assert_equal board_result['project']['name'], board.project.name
+  end
+end
