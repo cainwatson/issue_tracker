5a2846385ae98daff957d25dc9c73007bb51e386
Cain Watson <cainwatson00@gmail.com>
2020-08-29 11:41:44 -0500

    feat: add board column type

---
+++ api/app/graphql/types/board_column_type.rb
@@ -0,0 +1,15 @@
+module Types
+  class BoardColumnType < Types::BaseObject
+    implements GraphQL::Types::Relay::Node
+
+    global_id_field :id
+
+    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+
+    field :name, String, null: false
+
+    field :user_creator, Types::UserType, null: false
+    field :board, Types::BoardType, null: false
+  end
+end
