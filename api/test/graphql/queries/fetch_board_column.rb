4ee874c4ce56c629417a3939d2c34be7a9644fe0
Cain Watson <cainwatson00@gmail.com>
2020-08-29 11:41:08 -0500

    test: fetch board column query

---
+++ api/test/graphql/queries/fetch_board_column.rb
@@ -0,0 +1,93 @@
+require 'test_helper'
+
+class FetchBoardColumnQueryTest < ActiveSupport::TestCase
+  test 'returns null with invalid id' do
+    query_string = <<-GRAPHQL
+      query fetchBoardColumn($id: ID!) {
+        node(id: $id) {
+          ...on BoardColumn {
+            id
+            createdAt
+            updatedAt
+            name
+          }
+        }
+      }
+    GRAPHQL
+
+    result = graphql_query(query_string, variables: { 'id' => 'invalid' })
+    column_result = result['data']['node']
+
+    assert_nil column_result
+  end
+
+  test 'returns column with id' do
+    query_string = <<-GRAPHQL
+      query fetchBoardColumn($id: ID!) {
+        node(id: $id) {
+          ...on BoardColumn {
+            id
+            createdAt
+            updatedAt
+            name
+          }
+        }
+      }
+    GRAPHQL
+
+    column = projects_board_columns(:tylers_one_todo)
+    result = graphql_query(query_string, variables: { 'id' => column.node_id })
+    column_result = result['data']['node']
+
+    assert column_result['id']
+    assert column_result['createdAt']
+    assert column_result['updatedAt']
+    assert_equal column_result['name'], column.name
+  end
+
+  test 'returns column with user creator' do
+    query_string = <<-GRAPHQL
+      query fetchBoardColumn($id: ID!) {
+        node(id: $id) {
+          ...on BoardColumn {
+            id
+            userCreator {
+              id
+            }
+          }
+        }
+      }
+    GRAPHQL
+
+    column = projects_board_columns(:tylers_one_todo)
+    result = graphql_query(query_string, variables: { 'id' => column.node_id })
+    column_result = result['data']['node']
+
+    assert column_result['userCreator']
+    assert column_result['userCreator']['id']
+  end
+
+  test 'returns column with board' do
+    query_string = <<-GRAPHQL
+      query fetchBoardColumn($id: ID!) {
+        node(id: $id) {
+          ...on BoardColumn {
+            id
+            board {
+              id
+              name
+            }
+          }
+        }
+      }
+    GRAPHQL
+
+    column = projects_board_columns(:tylers_one_todo)
+    result = graphql_query(query_string, variables: { 'id' => column.node_id })
+    column_result = result['data']['node']
+
+    assert column_result['board']
+    assert column_result['board']['id']
+    assert_equal column_result['board']['name'], column.board.name
+  end
+end
