f75bd801e5d50e769006cbfc374aa2c18a258718
Cain Watson <cainwatson00@gmail.com>
2020-08-29 11:41:56 -0500

    feat: implement create board column mutation

---
+++ api/app/graphql/mutations/create_board_column.rb
@@ -0,0 +1,30 @@
+module Mutations
+  class CreateBoardColumn < Mutations::BaseMutation
+    null true
+
+    argument :name, String, required: true
+    argument :user_creator_id, ID, required: true
+    argument :board_id, ID, required: true
+
+    field :column, Types::BoardColumnType, null: true
+    field :errors, [String], null: false
+
+    def resolve(**args)
+      user_creator = IssueTrackerSchema.object_from_id(args[:user_creator_id])
+      board = IssueTrackerSchema.object_from_id(args[:board_id])
+
+      column = Projects::BoardColumn.create(
+        name: args[:name],
+        user_creator: user_creator,
+        board: board
+      )
+
+      return { errors: column.errors.full_messages } if column.invalid?
+
+      {
+        column: column,
+        errors: []
+      }
+    end
+  end
+end
