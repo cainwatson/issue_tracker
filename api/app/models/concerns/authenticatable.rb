04bca4a1cbcfd0e67ed09ff8b1d5847f4d7b8751
Cain Watson <cainwatson00@gmail.com>
2020-09-04 11:06:23 -0500

    feat: create authenticatable helper

---
+++ api/app/models/concerns/authenticatable.rb
@@ -0,0 +1,67 @@
+module Authenticatable
+  extend ActiveSupport::Concern
+
+  included do
+    attr_accessor :password
+
+    before_save :hash_password
+    before_create :initialize_jti
+
+    validates :email, presence: true, uniqueness: true
+    validates :password, presence: true, length: { minimum: 8 }
+  end
+
+  class_methods do
+    def from_jwt(jwt)
+      decoded_array = Authenticatable.decode_jwt(jwt)
+      payload = decoded_array.first
+      user = find(payload['sub'])
+
+      user if user['jti'] == payload['jti']
+    rescue ActiveRecord::RecordNotFound
+      nil
+    rescue JWT::DecodeError
+      nil
+    end
+  end
+
+  def self.decode_jwt(jwt)
+    JWT.decode(jwt, Authenticatable.jwt_secret, true, { algorithm: 'HS256' })
+  end
+
+  def self.jwt_secret
+    ENV['JWT_SECRET']
+  end
+
+  def jwt
+    payload = { sub: id, jti: jti }
+    JWT.encode(payload, Authenticatable.jwt_secret, 'HS256')
+  end
+
+  def valid_jwt?(jwt)
+    decoded_array = Authenticatable.decode_jwt(jwt)
+    payload = decoded_array.first
+    payload['jti'] == jti
+  rescue JWT::VerificationError
+    false
+  end
+
+  def revoke_jwt
+    update_column(:jti, generate_jti)
+  end
+
+  private
+
+  def hash_password
+    hasher = Argon2::Password.new
+    self.password_digest = hasher.create(password)
+  end
+
+  def initialize_jti
+    self.jti = generate_jti
+  end
+
+  def generate_jti
+    SecureRandom.uuid
+  end
+end
