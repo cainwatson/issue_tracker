1ced745bfba3772c9843f9a0db62b37e8862affe
Cain Watson <cainwatson00@gmail.com>
2020-09-08 16:16:29 -0500

    test: user authentication checks

--- api/test/models/accounts/user_test.rb
+++ api/test/models/accounts/user_test.rb
@@ -2,8 +2,42 @@ require 'test_helper'
 
 module Accounts
   class UserTest < ActiveSupport::TestCase
-    # test "the truth" do
-    #   assert true
-    # end
+    test 'validates password' do
+      user = Accounts::User.create(email: 'test@test.test', password: 'testpass')
+      assert_not user.valid?
+    end
+
+    test 'hashes and discards password' do
+      password = 'i5uTwEdAJ0HMg4cXMEf4f'
+      user = Accounts::User.create(email: 'test@test.test', password: password)
+      assert_nil user.password
+      assert user.password_digest
+      assert_not_equal user.password_digest, password
+    end
+
+    test 'generates jti on creation' do
+      user = Accounts::User.create(email: 'test@test.test', password: 'i5uTwEdAJ0HMg4cXMEf4f')
+      assert user.jti
+    end
+
+    test 'create revokable jwt' do
+      user = Accounts::User.create(email: 'test@test.test', password: 'i5uTwEdAJ0HMg4cXMEf4f')
+      assert user.jwt
+
+      old_jwt = user.jwt
+      user.revoke_jwt
+      new_jwt = user.jwt
+
+      assert_not_equal new_jwt, old_jwt
+      assert_not user.valid_jwt?(old_jwt)
+      assert user.valid_jwt?(new_jwt)
+    end
+
+    test 'return user from jwt' do
+      user = Accounts::User.create(email: 'test@test.test', password: 'i5uTwEdAJ0HMg4cXMEf4f')
+      returned_user = Accounts::User.from_jwt(user.jwt)
+
+      assert_equal returned_user, user
+    end
   end
 end
