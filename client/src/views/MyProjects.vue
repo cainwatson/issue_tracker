695094bd828ac9ae9986cce2ade957f55ebf6df7
Cain Watson <cainwatson00@gmail.com>
2020-09-15 17:41:22 -0500

    feat: create my projects component

---
+++ client/src/views/MyProjects.vue
@@ -0,0 +1,46 @@
+<template>
+  <section>
+    <h1>Projects</h1>
+    <ul
+      v-if="projects"
+      class="uk-child-width-1-2@m uk-child-width-1-3@l"
+      uk-grid
+    >
+      <li
+        v-for="project in projects"
+        :key="project.id"
+        class="uk-card  uk-card-body uk-card-muted"
+      >
+        {{ project.name }}
+      </li>
+    </ul>
+  </section>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { useQuery, useResult } from '@vue/apollo-composable'
+import gql from 'graphql-tag'
+
+export default defineComponent({
+  name: 'MyProjects',
+  setup() {
+    const { result, loading } = useQuery(gql`
+      query {
+        users {
+          projects {
+            id
+            name
+          }
+        }
+      }
+    `)
+    const projects = useResult(result, [], data => data.users[0].projects)
+
+    return {
+      loading,
+      projects,
+    }
+  },
+})
+</script>
