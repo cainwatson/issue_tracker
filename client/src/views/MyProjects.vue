753c116fc76cbc83f1199c1464bc17958a39ae0a
Cain Watson <cainwatson00@gmail.com>
2020-09-29 23:17:47 -0500

    feat: add link to individual project in my projects view

--- client/src/views/MyProjects.vue
+++ client/src/views/MyProjects.vue
@@ -6,12 +6,15 @@
       class="uk-child-width-1-2@m uk-child-width-1-3@l"
       uk-grid
     >
-      <li
-        v-for="project in projects"
-        :key="project.id"
-        class="uk-card  uk-card-body uk-card-muted"
-      >
-        {{ project.name }}
+      <li v-for="project in projects" :key="project.id">
+        <router-link
+          :to="`/projects/${project.id}`"
+          class="uk-link-toggle uk-link-reset"
+        >
+          <article class="uk-card uk-card-default uk-card-body uk-card-muted">
+            <h1 class="uk-text-large">{{ project.name }}</h1>
+          </article>
+        </router-link>
       </li>
     </ul>
   </section>
