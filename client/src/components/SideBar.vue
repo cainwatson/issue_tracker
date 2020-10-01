65e33bf54c6503f47331540f41c957012205cc24
Cain Watson <cainwatson00@gmail.com>
2020-09-30 19:48:18 -0500

    feat: move dashboard link on sidebar

--- client/src/components/SideBar.vue
+++ client/src/components/SideBar.vue
@@ -3,11 +3,16 @@
     class="uk-width-medium uk-padding uk-light uk-background-secondary"
     uk-height-viewport
   >
-    <router-link to="/dashboard" class="uk-link-reset">
+    <router-link to="/" class="uk-link-reset">
       <h1 class="uk-text-center uk-margin-medium-bottom">Issue Tracker</h1>
     </router-link>
     <nav>
       <ul class="uk-list uk-list-large">
+        <li>
+          <router-link to="/dashboard" class="uk-button uk-width-1-1">
+            Dashboard
+          </router-link>
+        </li>
         <li>
           <router-link to="/me/projects" class="uk-button uk-width-1-1">
             My Projects
