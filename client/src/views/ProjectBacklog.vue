b6913a0be3f3ac106ab3d6fadb72a758707ead62
Cain Watson <cainwatson00@gmail.com>
2020-10-04 22:39:45 -0500

    feat: add text if no issues found

--- client/src/views/ProjectBacklog.vue
+++ client/src/views/ProjectBacklog.vue
@@ -1,28 +1,31 @@
 <template>
   <article v-if="loading" uk-spinner />
-  <section
-    v-else
-    uk-accordion
-    multiple
-    targets="ul > *"
-    toggle="> article > .uk-accordion-title"
-    content="> article > .uk-accordion-content"
-  >
-    <ul v-if="issues.length > 0" class="uk-list uk-list-striped">
-      <li v-for="issue in issues" :key="issue.id">
-        <article>
-          <a class="uk-accordion-title uk-flex" href="#">
-            <span class="uk-margin-small-right">{{ issue.number }}</span>
-            <p class="uk-margin-remove">
-              {{ issue.summary }}
+  <section v-else>
+    <p v-if="issues.length === 0">No issues found.</p>
+    <div
+      v-else
+      uk-accordion
+      multiple
+      targets="ul > *"
+      toggle="> article > .uk-accordion-title"
+      content="> article > .uk-accordion-content"
+    >
+      <ul class="uk-list uk-list-striped">
+        <li v-for="issue in issues" :key="issue.id">
+          <article>
+            <a class="uk-accordion-title uk-flex" href="#">
+              <span class="uk-margin-small-right">{{ issue.number }}</span>
+              <p class="uk-margin-remove">
+                {{ issue.summary }}
+              </p>
+            </a>
+            <p class="uk-accordion-content uk-margin-remove-bottom">
+              {{ issue.description }}
             </p>
-          </a>
-          <p class="uk-accordion-content uk-margin-remove-bottom">
-            {{ issue.description }}
-          </p>
-        </article>
-      </li>
-    </ul>
+          </article>
+        </li>
+      </ul>
+    </div>
   </section>
 </template>
 
