5374a3743f719f57cebe8d8c2342d41eaca914c9
Cain Watson <cainwatson00@gmail.com>
2020-10-09 11:56:36 -0500

    feat: add ability to move board items to other columns

--- api/app/graphql/mutations/move_board_item.rb
+++ api/app/graphql/mutations/move_board_item.rb
@@ -1,15 +1,23 @@
 module Mutations
   class MoveBoardItem < Mutations::BaseMutation
-    description 'Move an item in a column to a new position.'
+    description 'Move an item to a new position or a new column.'
 
     argument :item_id, ID, required: true, loads: Types::BoardItemType
+    argument :column_id, ID, required: false, loads: Types::BoardColumnType
     argument :position, Int, required: true
 
     field :item, Types::BoardItemType, null: true
     field :errors, [String], null: true
 
-    def resolve(item:, position:)
-      item.update(column_order_position: position)
+    def resolve(item:, position:, **args)
+      if args[:column]
+        item.update(
+          column_order_position: position,
+          column: args[:column]
+        )
+      else
+        item.update(column_order_position: position)
+      end
 
       if item.invalid?
         { errors: item.errors.full_messages }
--- api/test/graphql/mutations/move_board_item_test.rb
+++ api/test/graphql/mutations/move_board_item_test.rb
@@ -32,4 +32,42 @@ class MoveBoardItemMutationTest < ActiveSupport::TestCase
     assert move_board_item_result['item']['updatedAt']
     assert_equal move_board_item_result['item']['columnPosition'], new_position
   end
+
+  test 'returns moved board item to another column' do
+    query_string = <<-GRAPHQL
+      mutation moveBoardItem($input: MoveBoardItemInput!) {
+        moveBoardItem(input: $input) {
+          errors
+          item {
+            id
+            createdAt
+            updatedAt
+            columnPosition
+            column {
+              id
+            }
+          }
+        }
+      }
+    GRAPHQL
+
+    item = projects_board_items(:tylers_one_todo_1)
+    new_column = projects_board_columns(:tylers_one_in_progress)
+    new_position = 0
+    input = {
+      'itemId' => item.node_id,
+      'columnId' => new_column.node_id,
+      'position' => new_position
+    }
+    result = graphql_query(query_string, variables: { 'input' => input })
+    move_board_item_result = result['data']['moveBoardItem']
+
+    assert_nil move_board_item_result['errors']
+
+    assert move_board_item_result['item']['id']
+    assert move_board_item_result['item']['createdAt']
+    assert move_board_item_result['item']['updatedAt']
+    assert_equal move_board_item_result['item']['columnPosition'], new_position
+    assert_equal move_board_item_result['item']['column']['id'], new_column.node_id
+  end
 end
